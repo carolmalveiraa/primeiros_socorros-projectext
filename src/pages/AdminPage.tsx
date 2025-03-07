1b27304a0076e43b43934c0982a8973196abf505
Carol Malveira <carolmalveiraa12@icloud.com>
2025-03-07 02:26:57 -0300

    Remove comentário de caminho de arquivo desnecessário em AdminPage.tsx

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -1,4 +1,3 @@
-// filepath: [AdminPage.tsx](http://_vscodecontentref_/3)
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
